43442869ab7b20296a0316bcb19ecfc5a0ea7baa
unknown <liehn.eduard1@gmail.com>
2023-07-16 16:17:45 +0300

    sure

---
+++ app/api/auth/[...nextauth]/route.js
@@ -0,0 +1,37 @@
+import NextAuth from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+import { connectToDB } from "@utils/database";
+import User from "@models/user";
+
+const handler = NextAuth({
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
+  ],
+  async session({ session }) {},
+  async signIn({ profile }) {
+    try {
+      //serverless ->lambda ->dynamodb
+      await connectToDB();
+
+      //check if a user already exists
+      const userExists = await User.findOne({ email: profile.email });
+
+      //if not create a new user and safe it to the DB
+      if (!userExists) {
+        await User.create({
+          email: profile.email,
+          username: profile.name.replace(" ", "").toLowerCase(),
+          image: profile.picture,
+        });
+      }
+    } catch (error) {
+      console.log(error);
+      return false;
+    }
+  },
+});
+
+export { handler as GET, handler as POST };
