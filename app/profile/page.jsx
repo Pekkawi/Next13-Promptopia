8c67bcc07c76657867ba63581650d6d0b7a0fc7f
unknown <liehn.eduard1@gmail.com>
2023-07-22 04:03:50 +0300

    added profile page

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -1,10 +1,42 @@
 "use client";
 
 import { useState, useEffect } from "react";
-import React from "react";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/router";
 
-const Profile = () => {
-  return <div>Profile</div>;
+import Profile from "@components/Profile";
+
+const MyProfile = () => {
+  const router = useRouter();
+  const { data: session } = useSession();
+
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const response = await fetch(`/api/users/${session?.user.id}/posts`);
+      const data = await response.json();
+
+      setPosts(data);
+    };
+
+    if (session?.user.id) fetchPosts();
+  }, []);
+
+  const handleEdit = (post) => {
+    router.push(`/update-prompt?id=${post._id}`);
+  };
+  const handleDelete = async (post) => {};
+
+  return (
+    <Profile
+      name="My"
+      desc="Welcome to your personalized Page"
+      data={posts}
+      handleEdit={handleEdit}
+      handleDelete={handleDelete}
+    />
+  );
 };
 
-export default Profile;
+export default MyProfile;
